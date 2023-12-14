79de41ff108eff95ab98ac79b43716742991c6dc
Flund <f.lundsten@hotmail.se>
2023-12-14 21:08:33 +0100

    Added css and html layout for the calculator. Added syntax for the logic, addition, subtraction, multiplication

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
-const body = document.querySelector('body');
+const buttons = document.querySelectorAll('button');
+const inputField = document.querySelector('input');
 
-let listOfSquares = [];
+let number = 0;
+let sum = 0;
+let combinator = '';
+let stack = [];
 
-addToListOfSquares(16);
+const setOfId = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']);
+const setOfOperand = new Set(['sub', 'add', 'multiply']);
 
+inputField.value = '';
 
-const mySquares = document.querySelectorAll('.square');
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        let theChosenOne = e.target;
 
-mySquares.forEach(square => {
-    square.addEventListener('mouseover', function (e){
-        e.target.style.backgroundColor = "yellow";
-    })
-})
-
-
-function addToListOfSquares(number) {
-    for (let j = 0; j < number; j++) {
-
-        const container = document.createElement('div');
-        container.classList.add('container');
-        body.appendChild(container);
-        container.style.display = "flex";
-
-        for (let i = 0; i < number; i++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            setStyleOfSquare(square);
-            container.appendChild(square);
+        if (stack.includes('equals')) {
+            inputField.value = '';
+            stack.length = 0;
         }
+        if (setOfId.has(theChosenOne.id)) {
+            inputField.value += theChosenOne.innerText;
+        } else if (theChosenOne.id === 'c') {
+            reset();
+            inputField.value = '';
+        } else if (setOfOperand.has(theChosenOne.id)) {
+            combinator = theChosenOne.id;
+            sum = parseInt(inputField.value);
+            inputField.value = '';
+
+        } else if (theChosenOne.id === 'equals') {
+            stack.push('equals');
+            number = parseInt(inputField.value);
+            calculateSum(combinator, sum, number);
+            reset();
+        }
+
+    })
+});
+
+function calculateSum(combinator, sum, number) {
+    switch (combinator) {
+        case 'sub':
+            inputField.value = sum - number;
+            break;
+        case 'add':
+            inputField.value = sum + number;
+            break;
+        case 'multiply':
+            inputField.value = sum * number;
+            break;
     }
 }
 
-function setStyleOfSquare(square) {
-    square.style.backgroundColor = "red";
-    square.style.width = "20px";
-    square.style.height = "20px";
+function reset() {
+    combinator = '';
+    sum = 0;
+    number = 0;
 }
